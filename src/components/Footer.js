0d63a99768daad788841455d52f196b9973ff177
5iveemmy <ainemmanuel87@gmail.com>
2021-10-18 16:29:32 +0100

    deleted icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,15 +28,9 @@ function Footer() {
                   />
                 </div>
                 <div className="row-two">
-<<<<<<< HEAD
                   {/* <span className="form-icon-one">
                     <i class="far fa-user-circle"></i>
                   </span> */}
-=======
-                  <span className="form-icon-one">
-                    <i class="far fa-user-circle"></i>
-                  </span>
->>>>>>> a7a53f7709e2be4a85731686c71ff82467e15fe1
                   <input
                     type="text"
                     placeholder="Last Name"
@@ -55,15 +49,9 @@ function Footer() {
                   />
                 </div>
                 <div className="row-two" id="phone-id">
-<<<<<<< HEAD
                   {/* <span className="form-icon-one">
                     <i class="fas fa-phone-alt"></i>
                   </span> */}
-=======
-                  <span className="form-icon-one">
-                    <i class="fas fa-phone-alt"></i>
-                  </span>
->>>>>>> a7a53f7709e2be4a85731686c71ff82467e15fe1
                   <input
                     placeholder="Phone"
                     type="tel"
@@ -73,15 +61,9 @@ function Footer() {
                 </div>
               </div>
               <div className="second-row">
-<<<<<<< HEAD
                 {/* <span className="form-icon-one">
                   <i class="far fa-clipboard"></i>
                 </span> */}
-=======
-                <span className="form-icon-one">
-                  <i class="far fa-clipboard"></i>
-                </span>
->>>>>>> a7a53f7709e2be4a85731686c71ff82467e15fe1
                 <input
                   placeholder="Subject"
                   type="text"
@@ -90,15 +72,9 @@ function Footer() {
                 />
               </div>
               <div className="text-row">
-<<<<<<< HEAD
                 {/* <span className="form-icon-one">
                   <i class="far fa-clipboard"></i>
                 </span> */}
-=======
-                <span className="form-icon-one">
-                  <i class="far fa-clipboard"></i>
-                </span>
->>>>>>> a7a53f7709e2be4a85731686c71ff82467e15fe1
                 <textarea
                   name="message"
                   placeholder="Message"
@@ -109,15 +85,9 @@ function Footer() {
                 ></textarea>
               </div>
               <div className="second-row">
-<<<<<<< HEAD
                 {/* <span className="form-icon-one">
                   <i class="far fa-clipboard"></i>
                 </span> */}
-=======
-                <span className="form-icon-one">
-                  <i class="far fa-clipboard"></i>
-                </span>
->>>>>>> a7a53f7709e2be4a85731686c71ff82467e15fe1
                 <input
                   placeholder="How did you hear about us?"
                   type="text"
