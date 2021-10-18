a7a53f7709e2be4a85731686c71ff82467e15fe1
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2021-10-18 16:19:36 +0100

    changes reverted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -62,7 +62,7 @@ function Footer() {
               </div>
               <div className="second-row">
                 <span className="form-icon-one">
-//                   <i class="far fa-clipboard"></i>
+                  <i class="far fa-clipboard"></i>
                 </span>
                 <input
                   placeholder="Subject"
@@ -73,7 +73,7 @@ function Footer() {
               </div>
               <div className="text-row">
                 <span className="form-icon-one">
-//                   <i class="far fa-clipboard"></i>
+                  <i class="far fa-clipboard"></i>
                 </span>
                 <textarea
                   name="message"
